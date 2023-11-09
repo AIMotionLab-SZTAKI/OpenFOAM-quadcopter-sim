15f59c963137ffc82b79b7ac86db20865edf2b27
Bendegúz Györök <119816494+gyorokbendeguz@users.noreply.github.com>
2023-11-09 15:27:12 +0100

    NCC creation from patches added
    - createPatchDict changed to create inner and outer patches
    - NonConformalCouplesDict changed accordingly

--- MRF_simulation/run_all.sh
+++ MRF_simulation/run_all.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
-cd ${0%/*} || exit 1    # Run from this directory
-
+cd "$(dirname "$0")" || exit 1  # Run from this directory
+# define paths
+DIR=$(pwd)  # current directory
+STATOR_MESH="$DIR/mesh_stator/MRF_stator.msh"
+ROTOR_MESH="$DIR/mesh_rotor/MRF_rotor.msh"
+#
 # Mesh stator part
-cd mesh_stator/
-fluentMeshToFoam MRF_stator.msh
-
+cd "$DIR/mesh_stator" || exit 1
+fluentMeshToFoam -case "$DIR/mesh_stator" "$STATOR_MESH"
+#
 # Mesh rotor part
-cd ../mesh_rotor/
-fluentMeshToFoam MRF_rotor.msh
-
-mkdir constant/polyMesh/sets
+cd "$DIR/mesh_rotor" || exit 1
+fluentMeshToFoam -case "$DIR/mesh_rotor" "$ROTOR_MESH"
+#
+mkdir "$DIR/mesh_rotor/constant/polyMesh/sets"
 topoSet
-
+#
 # Merge meshes
-cd ../
-mergeMeshes -overwrite mesh_stator mesh_rotor
-
-rm -r constant/polyMesh/
-cp -r mesh_stator/constant/polyMesh/ constant/
-
-checkMesh
-
-createPatch -overwrite
-
-topoSet
-
-decomposePar
-
-mpirun -np 4 simpleFoam -parallel | tee log.simple
+cd $DIR || exit 1
+#mergeMeshes -overwrite mesh_stator mesh_rotor
+#
+#rm -r $(DIR)/constant/polyMesh/
+#cp -r $(DIR)/mesh_stator/constant/polyMesh/ constant/
+#
+#checkMesh
+#
+#createPatch -overwrite
+#
+#topoSet
+#
+#decomposePar
+#
+#mpirun -np 4 simpleFoam -parallel | tee log.simple
